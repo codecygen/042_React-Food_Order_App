4a837c6e1e944d90d95eebbaed201f3009745ef2
gingercook <vahitaras@gmail.com>
2021-12-09 01:01:26 -0500

    input and button components added

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,20 +1,20 @@
 import React from 'react';
-
+import MealItemForm from '../MealItemForm';
 import classes from './MealItem.module.css';
 
 const MealItem = props => {
 
     return (
-       <li className={classes.meal}>
-           <div>
-               <h3>{props.name}</h3>
-               <div className={classes.description}>{props.description}</div>
-               <div className={classes.price}>${props.price.toFixed(2)}</div>
+        <li className={classes.meal}>
+            <div>
+                <h3>{props.name}</h3>
+                <div className={classes.description}>{props.description}</div>
+                <div className={classes.price}>${props.price.toFixed(2)}</div>
             </div>
-           <div>
-
-           </div>
-       </li> 
+            <div>
+                <MealItemForm id={props.id} />
+            </div>
+        </li>
     );
 };
 
