95041ab80ce8935ce426b58a7ae4b7a533c53534
gingercook <vahitaras@gmail.com>
2021-12-13 21:43:38 -0500

    useEffect logic added

--- README.md
+++ README.md
@@ -20,8 +20,9 @@ Searchable Sections:
 - React-useRef-useStateAlternativeNoReRender-forwardRef  
     "MealItemForm.js" will forward the ref to the children component "Input.js".  
     Input we receive from "ref" prop will always be in String format even if it is a number. In order to convert it into a Number type variable, put plus sign in front of it.  
-- React-useEffect-UsingSideEffects  
+- React-useEffect-useState-HeaderCartButtonBumpAnimation  
     The idea to use useEffect hook is to execute code that needs happens during lifecycle of the component instead of on specific user interactions or DOM events.  
+    In this example, we will use useEffect to deploy a bump animation for "HeaderCartButton.js" component. So that it will have a bump animation when we add a new item to the cart. In order to do that, useEffect and useState have to be used at the same time.  
     Refer to this link [here](https://github.com/codecygen/040_React-Side_Effects-Reducers-Context_API) for more info on debouncing and other useEffect examples.  
 - Javascript-bindMethod  
     This method ensures that the given arguments will be taken into the called functions.  
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,5 +1,6 @@
 // React-ContextAPI-CentralizingProps
-import React, {useContext} from 'react';
+// React-useEffect-useState-HeaderCartButtonBumpAnimation
+import React, {useContext, useEffect, useState} from 'react';
 import CartIcon from '../Cart/CartIcon';
 // React-ContextAPI-CentralizingProps
 import CartContext from '../../store/cart-context';
@@ -7,24 +8,40 @@ import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
+    // React-useEffect-useState-HeaderCartButtonBumpAnimation
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
     // React-ContextAPI-CentralizingProps
     const cartCtx = useContext(CartContext);
 
-    // cartCtx.items is an array of objects of items. We need to only extract the amounts here.
-    const amount = [];
-    // React-ContextAPI-CentralizingProps
-    cartCtx.items.forEach(item => amount.push(item.amount));
+    // React-useEffect-useState-HeaderCartButtonBumpAnimation
+    const { items } = cartCtx;
 
     // We use reduce method because a person can buy multiple of same item.
     // So we need to add all item numbers added on top of each other.
     // React-ContextAPI-CentralizingProps
-    const numberOfCartItems = amount.reduce((previousValue, currentValue) => {
-        return (
-            previousValue + currentValue
-        );
-    }, 0);
+    const numberOfCartItems = items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+      }, 0);
 
-    const btnClasses = `${classes.button} ${classes.bump}`;
+    // React-useEffect-useState-HeaderCartButtonBumpAnimation
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    // React-useEffect-useState-HeaderCartButtonBumpAnimation
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+          }
+          setBtnIsHighlighted(true);
+      
+          const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+          }, 300);
+      
+          return () => {
+            clearTimeout(timer);
+          };
+    }, [items]);
 
     return (
         <button className={btnClasses} onClick={props.onClick}>
