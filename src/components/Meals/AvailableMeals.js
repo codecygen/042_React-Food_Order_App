ed53c5ca74f9aa7dfeda8ecafe3ee03f2fdd9e9f
gingercook <vahitaras@gmail.com>
2022-03-14 22:06:26 -0400

    isloading state added. End of the day with React course

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -15,6 +15,13 @@ const AvailableMeals = () => {
     const [meals, setMeals] = useState([]);
 
     // Fetch-API-Fetch-Data-Firebase
+    // Because fetching the data from an API will take some time, 
+    // we need to introduce a loading state to let the person know 
+    // if we could have fetched a data and/or if our app is still busy 
+    //getting the data from the external server (API). 
+    const [isLoading, setIsLoading] = useState(true);
+
+    // Fetch-API-Fetch-Data-Firebase
     // useEffect function should not return a promise. Instead create another function if you need
     // to return a promise.
     useEffect(() => {
