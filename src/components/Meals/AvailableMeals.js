ad31db80dde35a1fb49f098217341d29de8d244a
gingercook <vahitaras@gmail.com>
2022-03-15 23:38:10 -0400

    error handling completed

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -34,10 +34,19 @@ const AvailableMeals = () => {
     useEffect(() => {
         // We create another nested function here which will return Promise.
         const fetchMeals = async () => {
+            const fetchLink = 'https://food-order-app-database-fa642-default-rtdb.firebaseio.com/meals.json';
+
             // This link is Firebase "Realtime Database" link
             // actual link is "https://food-order-app-database-fa642-default-rtdb.firebaseio.com/"
             // "meals.json" section is coming from the collection name "meals".
-            const res = await fetch('https://food-order-app-database-fa642-default-rtdb.firebaseio.com/meals.json');
+            const res = await fetch(fetchLink);
+
+            // Fetch-API-Fetch-Data-Firebase
+            // Fetch-API-Fetch-Data-Error-Handling-Firebase
+            if (!res.ok) {
+                throw new Error('Something went wrong');
+            }
+
             const mealData = await res.json();
 
             const loadedMeals = [];
@@ -55,7 +64,27 @@ const AvailableMeals = () => {
             setIsLoading(false);
         };
 
-        fetchMeals();
+        // Fetch-API-Fetch-Data-Firebase
+        // Fetch-API-Fetch-Data-Error-Handling-Firebase
+
+        // fetchMeals is an async function. Therefore
+        // it will return promise. If you throw an error inside
+        // the promise, that promise will cause the error to reject
+        // This is why we need to make it async here.
+        // If you don't put it, error won't show up
+        // It will only show the situation as "loading" on the
+        // webpage in case it has an error.
+        // If you want to see the error case, alter the fetch link
+        // tand remove "meals.json" section.
+        fetchMeals().catch(error => {
+            // Still need to set loading to false
+            // Because here this is the error case
+            // try case already has its own setIsloading
+            // set to false for the function fetchMeals()
+            setIsLoading(false);
+            setHttpError(error.message);
+        });
+
 
     }, []);
 
@@ -64,7 +93,17 @@ const AvailableMeals = () => {
     if (isLoading) {
         return (
             <section className={classes['meals-loading']}>
-                <p>Loading... </p>
+                <p>Loading...</p>
+            </section>
+        );
+    }
+
+    // Fetch-API-Fetch-Data-Firebase
+    // Fetch-API-Fetch-Data-Error-Handling-Firebase
+    if (httpError) {
+        return (
+            <section className={classes['meals-error']}>
+                <p>{httpError}</p>
             </section>
         );
     }
